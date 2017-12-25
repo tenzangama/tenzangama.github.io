3ef192b91502a2518ca1a2dabcabaada211ab9da
Shuntaro Katsuda <shunten3@gmail.com>
2017-12-25 11:53:08 +0900

    initial commit

---
+++ js/countdown.js
@@ -0,0 +1,27 @@
+﻿date = new Date()
+
+var invitation_date = new Date(2017, 3, 10);
+
+var remain_invitation_date = Math.floor((invitation_date - date) / (24 * 60 * 60 * 1000));
+
+remain_invitation_date += 1;
+
+if (remain_invitation_date >= 1) {
+    document.getElementById("invitation_time").innerHTML = "募集開始まで残り" + remain_invitation_date + "日！"
+} else {
+    document.getElementById("invitation_time").innerHTML = "現在募集中です"
+};
+
+    function setCounter(year, month, day, key, end_message){
+        var target_date = new Date(year, month-1, day);
+        var remain_days = Math.floor((target_date-date)/(24*60*60*1000));
+        remain_days += 1;
+        if (end_message == null){
+            end_message = "終了しました。"
+        }
+        if(remain_days >= 1){
+            document.getElementById(key).innerHTML = "あと"+ remain_days + "日";
+        }else{
+            document.getElementById(key).innerHTML = end_message;
+        }
+    };
---
+++ js/index.js
@@ -0,0 +1,15 @@
+/**
+ * Created by admin on 2017/10/24.
+ */
+
+//var image_num = 6
+//var num = Math.floor(Math.random() * image_num)
+
+//document.getElementById('top-image').style.backgroundImage = "url('../images/masuda.jpg')";
+//$('#top-image').css('background-image', 'http://kemono-friendsch.com/wp-content/uploads/2017/02/5V4Btww.jpg');
+//switch(num){
+//    case 0:
+//        document.getElementsByClassName('wide').style.backgroundImage = "../images/masuda.jpg"
+//
+//}
+
